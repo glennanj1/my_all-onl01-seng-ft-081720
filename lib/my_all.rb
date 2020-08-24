ac1e93de9936d3d9eef5405b93c6f58ab307c56e
John Glennan <glennan20@gmail.com>
2020-08-24 19:27:36 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,11 +4,16 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length 
-    yield(collection[i])
+    block return_values << yield(collection[i])
     i += 1
   end
-  collection
+  
+  if block_return_values.include?(false)
+    false
+  else
+    true 
+  end 
+  
 
 end
 
-my_all?([1,2,3]) {|i| i < 2}
